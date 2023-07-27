430e1c855592f7caff92b999c5481b3c8d1948dd
JonasFortes12 <jonascforte@hotmail.com>
2023-07-27 00:17:12 -0300

    style: add image on main section

--- src/pages/InitialPage/index.jsx
+++ src/pages/InitialPage/index.jsx
@@ -1,17 +1,24 @@
 import { Fragment } from "react";
 import Header from "../../components/Header";
 import Menu from "../../components/Menu";
-
+import banner from "../../assets/banner.png";
+import "./initialPage.css";
 
 function InitialPage() {
-    return(
-        <Fragment>
-            <Header/>
-            <Menu/>
-            
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <section className="mainSection">
+          <Menu />
+          <div className="image">
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="Imagem da terra" />
+          </div>
+        </section>
+      </main>
+    </Fragment>
+  );
 }
-
 
 export default InitialPage;
